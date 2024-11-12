4f3fe1b7ad13c20e879942f7531ec43f593d4029
Jayasri24-sri <melpadijayasri2001@gmail.com>
2024-11-12 08:59:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
